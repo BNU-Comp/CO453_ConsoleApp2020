fd95ae68f7a52d721a2bf2f201ca7cbdd4ea342e
DerekPeacock <derek.peacock@yahoo.com>
2020-02-06 11:02:59 +0000

    Refactored to create a general UI class and test methods in the main program

---
+++ CO453_ConsoleApp2020/ConsoleUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CO453_ConsoleApp2020
+{
+    /// <summary>
+    /// This class will provide general methods to assist user
+    /// input and output using the Console
+    /// </summary>
+    public static class ConsoleUI
+    {
+        public static void WriteTitle(string title, string task)
+        {
+            Console.WriteLine("==========================================");
+            Console.WriteLine("  CO453 Class Exercises by Derek Peacock  ");
+            Console.WriteLine("  " + task + " " + title);
+            Console.WriteLine("==========================================");
+            Console.WriteLine();
+        }
+    }
+}
--- CO453_ConsoleApp2020/Program.cs
+++ CO453_ConsoleApp2020/Program.cs
@@ -7,10 +7,18 @@ namespace CO453_ConsoleApp2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("===================================");
-            Console.WriteLine("  Convert Miles to Feet by Derek   ");
-            Console.WriteLine("===================================");
-            Console.WriteLine();
+            TestDistanceConverter();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Task 4.1
+        /// Test that the DistanceConverter can accurately convert
+        /// any number of miles into feet and vice versa
+        /// </summary>
+        private static void TestDistanceConverter()
+        {
+            ConsoleUI.WriteTitle("Distance Converter", "Task 4.1");
 
             T41_DistanceConverter converter = new T41_DistanceConverter();
 
@@ -18,8 +26,6 @@ namespace CO453_ConsoleApp2020
             double feet = converter.ToFeet(miles);
 
             Console.WriteLine("No of Feet = " + feet);
-            Console.ReadLine();
-
         }
     }
 }
